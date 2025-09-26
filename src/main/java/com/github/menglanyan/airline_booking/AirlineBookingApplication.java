a7eec1a0626e2cf0080e4b6b460d285d0635999b
menglan <menglan_yan@outlook.com>
2025-09-26 21:17:32 +0100

    feat: enable async processing for email notifications

--- src/main/java/com/github/menglanyan/airline_booking/AirlineBookingApplication.java
+++ src/main/java/com/github/menglanyan/airline_booking/AirlineBookingApplication.java
@@ -2,12 +2,14 @@ package com.github.menglanyan.airline_booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AirlineBookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AirlineBookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineBookingApplication.class, args);
+    }
 
 }
